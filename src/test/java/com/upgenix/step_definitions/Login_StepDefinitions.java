e782651e6bc1d8f3c49339923b8af72bf278e4f0
Nrfsnozdemir <avnurefsanozdemir@gmail.com>
2022-07-01 14:04:28 +0100

    Login_StepDefinitions created

---
+++ src/test/java/com/upgenix/step_definitions/Login_StepDefinitions.java
@@ -0,0 +1,25 @@
+package com.upgenix.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login_StepDefinitions {
+
+    @Given("user is on the Upgenix login page")
+    public void user_is_on_the_upgenix_login_page() {
+
+    }
+    @When("{string} enters valid {string} and valid {string}")
+    public void enters_valid_and_valid(String string, String string2, String string3) {
+
+    }
+    @When("user clicks to {string} button")
+    public void user_clicks_to_button(String string) {
+
+    }
+    @Then("user should see the title is {string}")
+    public void user_should_see_the_title_is(String string) {
+
+    }
+}
