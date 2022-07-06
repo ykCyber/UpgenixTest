2018bb3770c33306108ce07b7b873f60f93f3e44
Nrfsnozdemir <avnurefsanozdemir@gmail.com>
2022-07-06 15:05:10 +0100

    Logout_Page edited

--- src/test/java/com/upgenix/pages/Logout_Page.java
+++ src/test/java/com/upgenix/pages/Logout_Page.java
@@ -1,4 +1,24 @@
 package com.upgenix.pages;
 
+import com.upgenix.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Logout_Page {
+
+    public Logout_Page(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(className = "oe_topbar_name")
+    public WebElement usernameIcon;
+
+    @FindBy(xpath = "//a[@data-menu='logout']")
+    public WebElement logOutBtn;
+
+    @FindBy(className = "modal-title")
+    public WebElement sessionExpiredMessage;
+
+
 }
