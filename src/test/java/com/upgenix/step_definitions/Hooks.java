2c954ed331b97833354dc0e2d5fd40aa290559f2
Nrfsnozdemir <avnurefsanozdemir@gmail.com>
2022-07-11 22:53:01 +0100

    Hooks

--- src/test/java/com/upgenix/step_definitions/Hooks.java
+++ src/test/java/com/upgenix/step_definitions/Hooks.java
@@ -9,27 +9,7 @@ import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
 
-    /*
-In the class we will be able to pass pre- & post- conditions to
- each scenario and each step
- */
 
-    //import from io.cucumber.java not from junit
-    //@Before (order = 1)
-//    @Before (order = 1)
-//    public void setupScenario(){
-//        System.out.println("====Setting up browser using cucumber @Before");
-//    }
-//
-//    @Before (value = "@login", order = 2)
-//    public void setupScenarioForLogins(){
-//        System.out.println("====this will only apply to scenarios with @login tag");
-//    }
-//
-//    @Before (value = "@db", order = 0)
-//    public void setupForDatabaseScenarios(){
-//        System.out.println("====this will only apply to scenarios with @db tag");
-//    }
 
     @After
     public void teardownScenario(Scenario scenario){
@@ -48,19 +28,9 @@ In the class we will be able to pass pre- & post- conditions to
         //BrowserUtils.sleep(5);
         Driver.closeDriver();
 
-        //System.out.println("====Closing browser using cucumber @After");
-        //System.out.println("====Scenario ended/ Take screenshot if failed!");
+
     }
 
-    //@BeforeStep
-    public void setupStep(){
-        System.out.println("-----------> applying setup using @BeforeStep");
-    }
-
-    //@AfterStep
-    public void afterStep(){
-        System.out.println("-----------> applying tearDown using @AfterStep");
-    }
 
 
 }
